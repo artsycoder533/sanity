af9be9a9959b117c306a3ffe27b8ebce4e9ff62b
Natasha Johnson <njohnson533@gmail.com>
2023-12-16 19:23:44 -0500

    wip

--- src/app/components/portfolio/Portfolio.tsx
+++ src/app/components/portfolio/Portfolio.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
-import { client } from '@/utils/configSanity'
-import { PortableText } from '@portabletext/react';
+import React from "react";
+import { client } from "@/utils/configSanity";
+import { PortableText } from "@portabletext/react";
 
 interface IPortfolio {
-    _id: string;
-    title: string;
-    description: any;
-    _createdAt: string;
+  _id: string;
+  title: string;
+  description: any;
+  _createdAt: string;
 }
 
-async function getData(){
-    const query = `*[_type == "portfolio"]`;
-    const data = await client.fetch(query);
-    return data as IPortfolio[];
+async function getData() {
+  const query = `*[_type == "portfolio"]`;
+  const data = await client.fetch(query);
+  return data as IPortfolio[];
 }
 
-const Portfolio = async() => {
-    const data = (await getData() as IPortfolio[]);
-    console.log(data);
+const Portfolio = async () => {
+  const data = (await getData()) as IPortfolio[];
+  console.log(data);
 
   return (
     <div>
-        <h1>Portfolio</h1>
-        <div className="space-y-4">
-            {data.map(item => (
-                <div key={item?._id} className='border p-6 w-full rounded-lg'>
-                    <div>
-                        <span>{item?.title}</span>
-                    </div>
-                    <div>
-                        <span>
-                            <PortableText value={item?.description} />
-                        </span>
-                    </div>
-                </div>
-            ))}
-        </div>
+      <h1>Portfolio</h1>
+      <div className="space-y-4">
+        {data.map((item) => (
+          <div key={item?._id} className="border p-6 w-full rounded-lg">
+            <div>
+              <span>{item?.title}</span>
+            </div>
+            <div>
+              <span>
+                <PortableText value={item?.description} />
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
